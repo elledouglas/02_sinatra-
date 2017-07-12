03dc304ad9a3f75a024db7313fb4aa10c0e7b54c
Kathleen <kathleenmcclay@gmail.com>
2017-07-12 15:18:55 -0400

    added redirect routes

--- server.rb
+++ server.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   # instructions for what to do when user visits '/home' will go here
 erb :index
 
+end
+
+get '/gallery' do
+redirects to('/portfolio')
 end
 
 get '/portfolio' do
@@ -12,5 +20,14 @@ erb :gallery
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+@interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+
+get '/favourites' do
+  @fav_links = [ 'github.com', 'slice.ca', 'bravotv.com', 'blogto.com', 'novel.com']
+
+erb :favourites
 end
